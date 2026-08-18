a2406f7f50a002a397b7d983cd1b8eb27be148c7
have_nothing_to_say <1391097580@qq.com>
2026-08-18 16:21:06 +0800

    测试glados-facility.com域名

--- checkin.py
+++ checkin.py
@@ -105,7 +105,7 @@ class Config:
     DEFAULT_VERBOSE = False
 
     """默认域名"""
-    DOMAINS = ["glados.one", "glados-facility.com", "glados.space", "glados.cloud", "railgun.info"]
+    DOMAINS = ["glados-facility.com", "glados.space", "glados.cloud", "glados.one", "railgun.info"]
 
     """兑换计划列表"""
     EXCHANGE_PLANS = {
